0a5e20ba4f732a95ba1366d0ecfe80914e177fe5
Eli <e.m.mills1995@gmail.com>
2022-05-04 06:41:40 -0400

    Added picture of schema and text file for query ideas. Starting writing DML queries, finished Genres, Locations, Companies, and Platforms.

--- project-step-3/DML.sql
+++ project-step-3/DML.sql
@@ -1,8 +1,84 @@
--- Users Queries
-    -- Create: "Sign Up" page   
-    -- Read: "Search stats" page, shows leaderboards or filters based on User search   
-    -- Update: "My Account" page    
-    -- Delete: "Delete my account" button   
+-- Users page
+
+-- Genres page
+
+-- -- Load Genres table
+SELECT * FROM Genres;
+
+-- -- Add new Genre
+INSERT INTO Genres (genre_name)
+VALUES (:name);
+
+-- -- Edit Genre
+UPDATE Genres 
+SET genre_name=:name 
+WHERE genre_id=:id;
+
+-- -- Delete Genre
+DELETE Genres
+WHERE genre_id=:id;
+
+
+
+-- Locations page
+
+-- -- Load Locations table
+SELECT * FROM Locations;
+
+-- -- Add new Location
+INSERT INTO Locations (city, state, country)
+VALUES (:city, :state, :country);
+
+-- -- Edit Location
+UPDATE Locations
+SET city=:city, state=:state, country=:country
+WHERE location_id=:id;
+
+-- -- Delete Location
+DELETE Locations
+WHERE location_id=:id;
+
+
+
+-- Companies page
+
+-- -- Load Companies table
+SELECT * FROM Companies
+JOIN Locations ON Companies.location_id=Locations.location_id;
+
+-- -- Add new Company
+INSERT INTO Companies (company_name, location_id)
+VALUES (:name, (SELECT location_id FROM Locations WHERE city=:city AND state=:state AND country=:country));
+
+-- -- Edit Company
+UPDATE Companies
+SET company_name=:name, location_id=(SELECT location_id FROM Locations WHERE city=:city AND state=:state AND country=:country)
+WHERE company_id=:id;
+
+-- -- Delete Company
+DELETE Companies
+WHERE company_id=:id;
+
+
+
+-- Platforms page
+
+-- -- Load Platforms table
+SELECT * FROM Platforms
+JOIN Companies ON Platforms.company_id=Companies.company_id;
+
+-- -- Add new Platform
+INSERT INTO Platforms (platform_name, company_id)
+VALUES (:name, (SELECT company_id FROM Companies WHERE company_name=:company_name));
+
+-- -- Edit Platform
+UPDATE Platforms
+SET platform_name=:name, company_id=(SELECT company_id FROM Companies WHERE company_name=:company_name)
+WHERE platform_id=:id;
+
+-- -- Delete Platform
+DELETE Platforms
+WHERE platform_id=:id;
 
 -- Games Queries
     -- Create: "Add Game to Database" form 
@@ -11,12 +87,6 @@
     -- Update: "Edit Game in Database" form
     -- Delete: "Remove Game from Database" button on Games tab
 
--- Genres Queries
-    -- Read: "Genre List" page, loads all Genres
-    -- Create: "Add Genre to Database" form on "Genre List" page
-    -- Update: "Edit Genre in Database" form on "Genre List" page
-    -- Delete: "Remove Genre from Database" button in "Genre List" page
-
 -- Platforms Queries
     -- Read: "Platform List" page, loads all platforms
     -- Create: "Add Platform to Database" form on "Platform List" page
@@ -24,21 +94,6 @@
     -- Read: "Games" tab, select all platforms for User to filter games
     -- Update: "Edit Platform in Database" form on "Platform List" page
     -- Delete: "Remove Platform from Database" button in "Platform List" page
-
--- Companies Queries
-    -- Read: "Company List" page, loads all Companies
-    -- Create: "Add Company to Database" form on "Company List" page
-    -- Read: "Add Game to Database" and "Edit Game in Database" forms, selects all Companies for dropdown
-    -- Read: "Add Platform to Database" and "Edit Platform in Database" forms, selects all Companies for dropdown
-    -- Update: "Edit Company in Database" form on "Company List" page
-    -- Delete: "Remove Company from Database" button in "Company List" page
-
--- Locations Queries
-    -- Read: "Location List" page, loads all Locations
-    -- Create: "Add Location to Database" form on "Location List" page
-    -- Read: "Add Company to Database" and "Edit Company in Database" forms, selects all Locations for dropdown
-    -- Update: "Edit Location in Database" form on "Location List" page
-    -- Delete: "Remove Location from Database" button in "Location List" page
 
 -- Playthroughs Queries
     -- Read: "Home" page and "Playthroughs" tab, loads all Playthroughs for a User, shows active before finished, most recent first
