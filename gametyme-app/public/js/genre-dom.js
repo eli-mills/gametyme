37f354a2283ae5bfff1be7ba31b34eeb0f3c0d7e
Elijah Mills <millse2@flip3.engr.oregonstate.edu>
2022-05-19 03:22:29 -0700

    Connected src js files to genres.hbs. Connected styles.css (moved into public directory).

---
+++ gametyme-app/public/js/genre-dom.js
@@ -0,0 +1,9 @@
+function showEdit(){
+    document.getElementById("editGenre").style.display = "block";
+}
+function showDelete(){
+    document.getElementById("deleteGenre").style.display = "block";
+}
+function addNew(){
+    document.getElementById("addGenre").style.display = "block";
+}
