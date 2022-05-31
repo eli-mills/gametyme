73544bab33f48a318ecc669f93c1aacf1b72d39f
Na Kim <kimn2@flip3.engr.oregonstate.edu>
2022-05-30 20:00:05 -0700

    added update Games

--- gametyme-app/public/js/games-dom.js
+++ gametyme-app/public/js/games-dom.js
@@ -1,6 +1,37 @@
-function showEdit(){
+function showEdit(selectedGame){
     document.getElementById("editGame").style.display = "block";
+    document.getElementById("selectgameName").value = selectedGame;
+    document.getElementById("selectgameSummary").value = selectedGame;
+    document.getElementById("selectgameDate").value = selectedGame;
+    document.getElementById("selectCompanyName").value = selectedGame;
+    document.getElementById("selectGenreName").value = selectedGame;
+    document.getElementById("selectplatName").value = selectedGame;
+   
+    let gameName = document.getElementById("selectgameName");
+    let gameTitle = gameName.options[gameName.selectedIndex].text;
+    document.getElementById("edit-game-name").value = gameTitle;
+
+    let gameSummary = document.getElementById("selectgameSummary");
+    let summary = gameSummary.options[gameSummary.selectedIndex].text;
+    document.getElementById("edit-game-summary").value = summary;
+
+    let gameDate = document.getElementById("selectgameDate");
+    let dateval= gameDate.options[gameDate.selectedIndex].text;
+    document.getElementById("edit-release-date").value = dateval;
+
+    let gameCompany = document.getElementById("selectCompanyName");
+    let company = gameCompany.options[gameCompany.selectedIndex].text;
+    document.getElementById("edit-game-company").value = company;
+
+    let gameGenre = document.getElementById("selectGenreName");
+    let genre = gameGenre.options[gameGenre.selectedIndex].text;
+    document.getElementById("edit-genre-name").value = genre;
+
+   
 }
+
+
+
 function showDelete(){
     document.getElementById("deleteGame").style.display = "block";
 }
