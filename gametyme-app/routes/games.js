50141b917340c677e935daca169d91ae58766988
Elijah Mills <millse2@flip3.engr.oregonstate.edu>
2022-05-30 18:25:33 -0700

    Got Add Game form to work. Wrote routes for Delete and Edit, but have not tested those yet. No front end for delete or edit yet.

--- gametyme-app/routes/games.js
+++ gametyme-app/routes/games.js
@@ -22,7 +22,6 @@ router.get('/', (req, res) => {
 
     const query = loadGames.concat(loadCompanies, loadGenres, loadPlatforms); 
 
-    console.log(query);
     db.query(query, (error, results, fields) => {
         if (error) throw error;
         res.render('games', {
@@ -32,36 +31,58 @@ router.get('/', (req, res) => {
             platforms:  results[3]
         });
         console.log('Games loaded');
-        console.log(results);
     });
 }); 
 
 
-// Add New Platform
+/** Add New Game 
+ *      Adds a new Game to the Games table with the given title, summary, release date,
+ *      company, and genre. Additionally, runs a loop for every given platform_id to create
+ *      new GamesPlatforms entries.
+ * 
+ * */ 
 
 router.post('/', (req, res) => { 
-    console.log(req.body);
-    const { addPlatformName, addCompanyId } = req.body;
-    const query = `
-        INSERT INTO Platforms (platform_name, company_id) 
-        VALUES ('${addPlatformName}', '${addCompanyId}');
+    console.log('Post request received: \n', req.body);
+    const { game_title, game_summary, release_date, company_id, genre_id, platform_ids } = req.body;
+    let query = `
+        INSERT INTO Games (game_title, game_summary, release_date, company_id, genre_id)
+        VALUES ('${game_title}', '${game_summary}', '${release_date}', ${company_id}, ${genre_id});
     `;
-    
+
     db.query(query, (error, results, fields) => {
         if (error){
             throw error;
-        }else{
-            res.redirect('/platforms');
+        } else {
+            // Game successfully added to Games table.
+            console.log('POST query successful: \n', results)
+
+
+            const game_id = results.insertId;
+            
+            // Create GamesPlatforms entries using newly-created game_id.
+            for (platform_id of platform_ids) {
+                gamePlatformQuery = `
+                INSERT INTO GamesPlatforms (game_id, platform_id)
+                VALUES (${game_id}, ${platform_id});
+                `;
+                console.log('Attempting query: \n', gamePlatformQuery);
+                db.query(gamePlatformQuery, (error, results, fields) => {if (error) throw error});
+            };
+
+
+            console.log('GamesPlatforms added.');
+            res.redirect('/games');
         }
     });
 });
 
-// Delete Platforms
-router.delete('/:platform_id', (req, res) => {
-    const platform_id = req.params.platform_id;
+// Delete Game
+router.delete('/:game_id', (req, res) => {
+    const game_id = req.params.game_id;
     const query = `
-        DELETE FROM Platforms
-        WHERE platform_id=${platform_id};
+        DELETE FROM Games
+        WHERE game_id=${game_id};
     `;
     
     db.query(query, (error, results, fields) => {
@@ -70,21 +91,81 @@ router.delete('/:platform_id', (req, res) => {
         }else{
             res.json(results);
             console.log(results);
-            console.log('Platform deleted');
+            console.log('Game deleted');
         }
     });
 });
 
-// Edit Platforms
-router.put('/:platform_id', (req, res) => {
-    const platform_id = req.params.platform_id;
-    const { platform_name, company_id } = req.body;
+/** Edit Game
+ *      Updates given game ID with given title, summary, release date, company id, and genre id.
+ *      Checks which GamesPlatforms need to be deleted and which need to be added, then executes
+ *      the appropriate queries.
+ * 
+ *      URL: /games/:game_id (where game_id is a number representing game to be updated)
+ * 
+ *      Request body: receives a Javascript object with the following properties (names must be exact):
+ *              game_title
+ *              game_summary
+ *              release_date
+ *              company_id
+ *              genre_id
+ *              platform_ids (this must be an array containing all selected platforms)
+ * 
+ * 
+ * 
+*/
+router.put('/:game_id', (req, res) => {
+    const game_id = req.params.game_id;
+    const { game_title, game_summary, release_date, company_id, genre_id, platform_ids } = req.body;
     const query = `
-        UPDATE Platforms
-        SET platform_name='${platform_name}', company_id=${company_id}
-        WHERE platform_id=${platform_id};
+        UPDATE Games
+        SET game_title='${game_title}', game_summary='${game_summary}', release_date='${release_date}', 
+            company_id=${company_id}, genre_id=${genre_id}
+        WHERE game_id=${game_id};    
     `;
 
+    // Get current GamesPlatforms
+    const gpQuery = `
+        SELECT platform_id FROM GamesPlatforms WHERE game_id=${game_id};
+    `;
+
+    let existingPlatforms = [];
+
+    db.query(gpQuery, (error, results, fields) => {
+        if (error) { 
+            throw error;
+        } else {
+            for ( let result of results ) {
+                existingPlatforms.append(result.platform_id);
+            }
+        }
+    });
+
+    // Find GamesPlatforms that have been deleted in update (in old and not in new)
+    const platsToDelete = existingPlatforms.filter(platform => {!(platform_ids.includes(platform))});
+
+    // Find GamesPlatforms that need to be added (in new and not in old)
+    const platsToAdd = platform_ids.filter(platform=>{!(existingPlatforms.includes(platform))});
+
+    // Delete appropriate GamesPlatforms
+    for (let platform_id of platsToDelete) {
+        const delQuery = `
+            DELETE FROM GamesPlatforms
+            WHERE game_id=${game_id} AND platform_id=${platform_id};
+        `
+        db.query(delQuery, (error, results, fields) => {if (error) throw error;});
+    }
+
+    // Add appropriate GamesPlatforms
+    for (let platform_id of platsToAdd) {
+        const addQuery = `
+            INSERT INTO GamesPlatforms (game_id, platform_id)
+            VALUES (${game_id}, ${platform_id});
+        `
+        db.query(addQuery, (error, results, fields) => {if (error) throw error;});
+    }
+
+    // Update rest of Games entry
     db.query(query, (error, results, fields) => {
         if (error) throw error;
 
